a55cd2de58e338fc829de8e6364ec60e51b9035c
Leopoldo Estevez <leopoldoestevezagra@gmail.com>
2022-08-14 11:41:23 +0200

    Added: Submit feedback on cta

--- index.js
+++ index.js
@@ -2,12 +2,18 @@ const form = document.getElementById('cta-form');
 form.addEventListener('submit', (e) => {
     e.preventDefault();
 
-    const email = document.getElementById('cta-email').value;
-    const content = document.getElementById('cta-body').value;
+    const email = document.getElementById('cta-email');
+    const content = document.getElementById('cta-body');
+    const buttonText = document.getElementById('cta-button');
+    const message = document.getElementById('cta-message');
 
-    const details = { email, content };
+    const details = { 
+        email: email.value, 
+        content: content.value 
+    };
 
-    console.warn(details);
+
+    buttonText.innerHTML = 'Sending...';
 
     fetch('/api/form', {
         method: 'POST',
@@ -17,6 +23,21 @@ form.addEventListener('submit', (e) => {
         },
         body: JSON.stringify(details)
 
+    }).then(() => {
+        email.value = '';
+        content.value = '';
+        message.innerHTML = 'Thanks you for submitting !'
+        message.style.opacity = 1;
+
+        setTimeout(() => {
+            message.innerHTML = '';
+            message.style.opacity = 0;
+        }, 5000);
+    }).finally(() => {
+        buttonText.innerHTML = `
+            Send
+            <img src="./assets/Arrow.svg" alt="arrow">
+        `
     });
 })
 
